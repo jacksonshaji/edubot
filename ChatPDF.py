ca8e24dce3a85310e80288f99c3483b42bcf85db
jacksonshaji <110402389+jacksonshaji@users.noreply.github.com>
2024-04-15 11:54:43 +0530

    Update ChatPDF.py

--- ChatPDF.py
+++ ChatPDF.py
@@ -84,7 +84,7 @@ def main():
         st.subheader("Your documents 📂")
         pdf_docs = st.file_uploader(
             "Upload your PDFs here and click on 'Process'", accept_multiple_files=True)
-        if st.button("Process"):
+        if st.button("Process ⚙"):
             with st.spinner("Processing"):
                 # get pdf text
                 raw_text = get_pdf_text(pdf_docs)
